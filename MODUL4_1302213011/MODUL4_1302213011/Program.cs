d9095028c017d4488c14096de6c7a18ed15f8ed6
PC18-IFLAB <61218518+Jannatin12@users.noreply.github.com>
2023-03-14 17:02:21 +0700

    menambahkan class PosisiKarakterGame

--- MODUL4_1302213011/MODUL4_1302213011/Program.cs
+++ MODUL4_1302213011/MODUL4_1302213011/Program.cs
@@ -14,10 +14,58 @@ public class KodeBuah
     }
 }
 
-class Program
+class PosisiKarakterGame
 {
+    enum State { Jongkok, Berdiri, Tengkurap, Terbang }
     static void Main(string[] args)
     {
-        Console.WriteLine("Kode buah Blackberry adalah {0:s}",KodeBuah.getKodeBuah(KodeBuah.Buah.Blackberry));
+        Console.WriteLine("Kode buah Blackberry adalah {0:s}\n", KodeBuah.getKodeBuah(KodeBuah.Buah.Blackberry));
+
+        State state = State.Berdiri;
+        string[] screen = { "Posisi Jongkok","Posisi Berdiri","Posisi Tengkurap","Posisi Terbang","Posisi landing","Posisi take off" };
+        while (state != null)
+        {
+            Console.WriteLine(screen[(int)state]);
+            Console.Write("Enter Command: ");
+            string command = Console.ReadLine();
+            switch (state)
+            {
+                case State.Berdiri:
+                    if (command == "TombolW")
+                    {
+                        state = State.Terbang;
+                        Console.WriteLine(screen[5]);
+                    }else if (command == "TombolS")
+                    {
+                        state = State.Jongkok;
+                    }
+                    break;
+                case State.Terbang:
+                    if(command == "TombolS")
+                    {
+                        state = State.Berdiri;
+                    }else if (command == "TombolX")
+                    {
+                        state = State.Jongkok;
+                        Console.WriteLine(screen[4]);
+                    }
+                    break;
+                case State.Jongkok:
+                    if(command == "TombolW")
+                    {
+                        state = State.Berdiri;
+                    }else if(command == "TombolS")
+                    {
+                        state = State.Tengkurap;
+                    }
+                    break;
+                case State.Tengkurap:
+                    if(command == "TombolW")
+                    {
+                        state = State.Jongkok;
+                    }
+                    break;
+            }
+        }
     }
 }
