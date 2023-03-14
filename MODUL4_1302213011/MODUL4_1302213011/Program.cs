cecfcadafdd8771d3b2b67c7fdc1f72ddc722689
PC18-IFLAB <61218518+Jannatin12@users.noreply.github.com>
2023-03-14 16:30:22 +0700

    menambahkan class KodeBuah

---
+++ MODUL4_1302213011/MODUL4_1302213011/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class KodeBuah
+{
+    public enum Buah
+    {
+        Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+    }
+
+    public static string getKodeBuah(Buah buah)
+    {
+        string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+        return kodeBuah[(int)buah];
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Kode buah Blackberry adalah {0:s}",KodeBuah.getKodeBuah(KodeBuah.Buah.Blackberry));
+    }
+}
